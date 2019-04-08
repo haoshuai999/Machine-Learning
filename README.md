91b76222070b07e288112d86474a5f468532482d
Hao Shuai <626593718@qq.com>
2019-04-08 14:23:12 -0400

    update readme file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Machine Learning algorithms
+# Machine Learning algorithms
 The problem1.py file uses perceptron learning algorithm to linearly seperate a dataset into two groups.
 The problem2.py file uses gradient descent to build a linear regression model for predicting.
 The problem3.py file process images by segmenting and grouping pixels of image.
---
+++ algorithms/problem2.py
@@ -0,0 +1,90 @@
+from visualize import visualize_3d
+import pandas as pd
+import sys
+
+def calculate_weight(df):
+	n = len(df)
+	alpha = [0.001, 0.005, 0.01, 0.05, 0.1, 0.5, 1, 5, 10, 1]
+	weights = []
+
+	for column in df:
+		mean = df.mean(axis = 0)[column]
+		std = df.std(axis = 0)[column]
+		for i in range(n):
+			df[column][i] = (df[column][i] - mean) / std
+
+
+	for i in range(len(alpha)):
+		beta = [0,0,0]
+		iteration = 0
+		while iteration < 100:
+			tempR = 0
+			tempbeta = [0,0,0]
+			iteration += 1
+			for j, row in df.iterrows():
+				diff_weight = beta[0] + (beta[1]*row[0]) + (beta[2]*row[1]) - row[2]
+				tempR += pow(diff_weight,2) 	
+				# beta[0] -= (alpha[i] / n) * diff_weight
+				# beta[1] -= (alpha[i] / n) * diff_weight * row[0]
+				# beta[2] -= (alpha[i] / n) * diff_weight * row[1]
+				tempbeta[0] += diff_weight
+				tempbeta[1] += diff_weight * row[0]
+				tempbeta[2] += diff_weight * row[1]
+
+
+			
+			R = tempR / (2 * n)
+			beta[0] -= (alpha[i] / n) * tempbeta[0]
+			beta[1] -= (alpha[i] / n) * tempbeta[1]
+			beta[2] -= (alpha[i] / n) * tempbeta[2]
+			#print([R,alpha[i]])
+		if i == 9:
+			while iteration < 116:
+				tempR = 0
+				tempbeta = [0,0,0]
+				iteration += 1
+				for j, row in df.iterrows():
+					diff_weight = beta[0] + (beta[1]*row[0]) + (beta[2]*row[1]) - row[2]
+					tempR += pow(diff_weight,2) 	
+					# beta[0] -= (alpha[i] / n) * diff_weight
+					# beta[1] -= (alpha[i] / n) * diff_weight * row[0]
+					# beta[2] -= (alpha[i] / n) * diff_weight * row[1]
+					tempbeta[0] += diff_weight
+					tempbeta[1] += diff_weight * row[0]
+					tempbeta[2] += diff_weight * row[1]
+
+
+				
+				R = tempR / (2 * n)
+				beta[0] -= (alpha[i] / n) * tempbeta[0]
+				beta[1] -= (alpha[i] / n) * tempbeta[1]
+				beta[2] -= (alpha[i] / n) * tempbeta[2]
+				print([R,alpha[i]])
+
+		weights.append([alpha[i],iteration,beta[0],beta[1],beta[2]])
+
+	return weights
+
+
+if __name__ == "__main__":
+	# Get input
+	input_file = sys.argv[1]
+	output_file = sys.argv[2]
+	#======== INPUT2.CSV =======#
+	print("Visualizing input2.csv")
+
+	# Import input1.csv, without headers for easier indexing
+	data = pd.read_csv(input_file, header=None)
+
+	# Process the data
+	result_weights = calculate_weight(data)
+	
+
+	# Draw the line
+	for i in range(len(result_weights)):
+		result = result_weights[i]
+		visualize_3d(data, lin_reg_weights=result[2:], alpha=result[0])
+
+	# Write to file
+	df = pd.DataFrame(result_weights)
+	df.to_csv(path_or_buf=output_file, header=['alpha','num_iters','bias','b_age','b_weight'], index=False)
